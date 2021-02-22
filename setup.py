f30f6a36b502c64176d687374ec92b331b24fca4
Uzay-G <uzgirit@gmail.com>
2021-02-22 15:49:39 +0100

    remove: unnecessary dep

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt', encoding='utf-8') as f:
 
 setup(
     name='archivy_static_site_gen',
-    version='0.1.0',
+    version='0.1.1',
     author="Uzay-G",
     author_email="halcyon@disroot.org",
     description=(
