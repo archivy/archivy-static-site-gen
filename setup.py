f5417093a64927daa0abf459e95eba78b4894843
Uzay-G <uzgirit@gmail.com>
2021-02-24 14:25:10 +0100

    v0.2.0: wiki desc, search

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt', encoding='utf-8') as f:
 
 setup(
     name='archivy_static_site_gen',
-    version='0.1.2',
+    version='0.2.0',
     author="Uzay-G",
     author_email="halcyon@disroot.org",
     description=(
